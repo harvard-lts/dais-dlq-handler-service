38f7c039eefb26703d5efced523cafe058ceca44
GPortas <hey@gportas.me>
2022-05-16 14:51:54 +0100

    Added: initial service structure with DLQ MQ listener

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask
+
+LOG_FILE_DEFAULT_PATH = "/home/hdhs/logs/hdhs.log"
+LOG_FILE_DEFAULT_LEVEL = logging.DEBUG
+LOG_FILE_MAX_SIZE_BYTES = 2 * 1024 * 1024
+LOG_FILE_BACKUP_COUNT = 1
+
+
+def create_app(config_name: str = 'default') -> Flask:
+    if config_name != 'testing':
+        configure_logger()
+
+    app = Flask(__name__)
+    return app
+
+
+def configure_logger() -> None:
+    log_file_path = os.getenv('LOG_FILE_PATH', LOG_FILE_DEFAULT_PATH)
+    logger = logging.getLogger()
+
+    file_handler = RotatingFileHandler(
+        filename=log_file_path,
+        maxBytes=LOG_FILE_MAX_SIZE_BYTES,
+        backupCount=LOG_FILE_BACKUP_COUNT
+    )
+    logger.addHandler(file_handler)
+
+    log_level = os.getenv('LOG_LEVEL', LOG_FILE_DEFAULT_LEVEL)
+    logger.setLevel(log_level)
---
+++ app/dlq/infrastructure/mq/listeners/transfer_dlq_listener.py
@@ -0,0 +1,22 @@
+import os
+
+from app.dlq.infrastructure.mq.mq_connection_params import MqConnectionParams
+from app.dlq.infrastructure.mq.listeners.stomp_listener_base import StompListenerBase
+
+
+class TransferDlqListener(StompListenerBase):
+
+    def _get_queue_name(self) -> str:
+        return os.getenv('MQ_PROCESS_QUEUE_PROCESS_DLQ')
+
+    def _get_mq_connection_params(self) -> MqConnectionParams:
+        return MqConnectionParams(
+            mq_host=os.getenv('MQ_TRANSFER_HOST'),
+            mq_port=os.getenv('MQ_TRANSFER_PORT'),
+            mq_ssl_enabled=os.getenv('MQ_TRANSFER_SSL_ENABLED'),
+            mq_user=os.getenv('MQ_TRANSFER_USER'),
+            mq_password=os.getenv('MQ_TRANSFER_PASSWORD')
+        )
+
+    def _handle_received_message(self, message_body: dict) -> None:
+        self._logger.info("Received message from Transfer Queue. Message body: " + str(message_body))
