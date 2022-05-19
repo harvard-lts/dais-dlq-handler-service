2238a34b855548bf28ef8b3923cbfa09c918d1a0
GPortas <hey@gportas.me>
2022-05-19 09:15:03 +0100

    Added: process resubmitting publisher and pending tests

---
+++ app/dlq/infrastructure/mq/publishers/process_resubmitting_publisher.py
@@ -0,0 +1,16 @@
+import os
+
+from app.dlq.infrastructure.mq.mq_connection_params import MqConnectionParams
+from app.dlq.infrastructure.mq.publishers.resubmitting_publisher_base import ResubmittingPublisherBase
+
+
+class ProcessResubmittingPublisher(ResubmittingPublisherBase):
+
+    def _get_mq_connection_params(self) -> MqConnectionParams:
+        return MqConnectionParams(
+            mq_host=os.getenv('MQ_PROCESS_HOST'),
+            mq_port=os.getenv('MQ_PROCESS_PORT'),
+            mq_ssl_enabled=os.getenv('MQ_PROCESS_SSL_ENABLED'),
+            mq_user=os.getenv('MQ_PROCESS_USER'),
+            mq_password=os.getenv('MQ_PROCESS_PASSWORD')
+        )
--- test/unit/dlq/infrastructure/mq/publishers/test_transfer_resubmitting_publisher.py
+++ test/unit/dlq/infrastructure/mq/publishers/test_transfer_resubmitting_publisher.py
@@ -6,29 +6,31 @@ from app.dlq.infrastructure.mq.publishers.transfer_resubmitting_publisher import
 
 class TestTransferResubmittingPublisher(TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.TEST_ORIGINAL_QUEUE = "test_queue"
+        cls.TEST_CURRENT_RETRY_COUNT = 2
+        cls.TEST_ORIGINAL_MESSAGE_BODY = {
+            "test": "test",
+            "admin_metadata": {
+                "original_queue": cls.TEST_ORIGINAL_QUEUE,
+                "retry_count": cls.TEST_CURRENT_RETRY_COUNT
+            }
+        }
+
     @patch("app.dlq.infrastructure.mq.publishers.stomp_publisher_base.StompPublisherBase._publish_message")
     def test_publish_message_happy_path(self, parent_publish_message_mock) -> None:
         sut = TransferResubmittingPublisher()
 
-        test_original_queue = "test_queue"
-        test_current_retry_count = 2
-        test_original_message_body = {
-            "test": "test",
-            "admin_metadata": {
-                "original_queue": test_original_queue,
-                "retry_count": test_current_retry_count
-            }
-        }
-
         sut.publish_message(
-            original_message_body=test_original_message_body,
-            current_retry_count=test_current_retry_count,
-            queue_name=test_original_queue
+            original_message_body=self.TEST_ORIGINAL_MESSAGE_BODY,
+            current_retry_count=self.TEST_CURRENT_RETRY_COUNT,
+            queue_name=self.TEST_ORIGINAL_QUEUE
         )
 
-        expected_message_body = test_original_message_body
-        expected_message_body["admin_metadata"]["retry_count"] = test_current_retry_count + 1
-        expected_queue_name = test_original_queue
+        expected_message_body = self.TEST_ORIGINAL_MESSAGE_BODY
+        expected_message_body["admin_metadata"]["retry_count"] = self.TEST_CURRENT_RETRY_COUNT + 1
+        expected_queue_name = self.TEST_ORIGINAL_QUEUE
         parent_publish_message_mock.assert_called_once_with(
             expected_message_body,
             expected_queue_name
