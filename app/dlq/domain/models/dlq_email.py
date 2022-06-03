5347ceeb8652c975e9226ad84b61a926fc9401c9
GPortas <hey@gportas.me>
2022-06-03 15:21:04 +0200

    Fixed: DLQ email subject template

--- app/dlq/domain/models/dlq_email.py
+++ app/dlq/domain/models/dlq_email.py
@@ -5,7 +5,7 @@ from app.common.domain.mailing.email import Email
 
 
 class DlqEmail(Email, ABC):
-    __SUBJECT_TEMPLATE = "HDC3A DLQ {} : {}"
+    __SUBJECT_TEMPLATE = "HDC3A DLQ {}: {}"
 
     def __init__(self) -> None:
         self.subject = self.__SUBJECT_TEMPLATE.format(self.__get_environment(), self.subject)
